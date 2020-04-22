8609d8eea077de62f57d5a23b61b16ad0cbf4481
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-22 11:17:48 +0700

    feat(config): add config parser

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type (
+	Config struct {
+		ConsulAddress string `json:"consul_address"`
+	}
+)
+
+func Parse() (config Config, err error) {
+	configPath := GetPath()
+
+	configFileContent, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(configFileContent, &config)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func GetPath() string {
+	environment := "DEV"
+	if environmentFromEnvVar := os.Getenv("ENVIRONMENT"); environmentFromEnvVar != "" {
+		environment = environmentFromEnvVar
+	}
+
+	configPath := "config/config-dev.json"
+	if configPathFromEnvVar := os.Getenv(environment + "_CONFIG_PATH"); configPathFromEnvVar != "" {
+		configPath = configPathFromEnvVar
+	}
+
+	return configPath
+}
