f9e113c119fe5b40702ca42d95cee5f963d2bc0c
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-23 03:57:44 +0700

    feat(transport): add search movie factory

--- transport/movie_server.go
+++ transport/movie_server.go
@@ -2,9 +2,23 @@ package transport
 
 import (
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/sd"
 	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/tech-showcase/api-gateway/model"
+	"github.com/tech-showcase/api-gateway/service"
+	"io"
 	"net/http"
 )
+
+func SearchMovieFactory() sd.Factory {
+	return func(instance string) (endpoint.Endpoint, io.Closer, error) {
+		movieClientEndpoints, conn := model.NewMovieGRPCClient(instance)
+		movieService := service.NewMovieService(movieClientEndpoints)
+		searchMovieEndpoint := makeSearchMovieHTTPEndpoint(movieService)
+
+		return searchMovieEndpoint, conn, nil
+	}
+}
 
 func NewMovieHTTPServer(endpoints MovieEndpoints) http.Handler {
 	m := http.NewServeMux()
