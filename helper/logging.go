57ffa1741c47f332844e59d8654d238ac4d9fb4a
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-23 03:24:44 +0700

    feat(helper): add logging helper

---
+++ helper/logging.go
@@ -0,0 +1,15 @@
+package helper
+
+import (
+	"github.com/go-kit/kit/log"
+	"os"
+)
+
+func NewLogger() log.Logger {
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(os.Stderr)
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+	logger = log.With(logger, "caller", log.DefaultCaller)
+
+	return logger
+}
