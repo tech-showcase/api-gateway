0baeae677a23ae3f292420c73dc06349ab5b78a2
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-23 04:28:58 +0700

    test(model): add test for decodeSearchMovieGRPCResponse

--- model/movie_test.go
+++ model/movie_test.go
@@ -39,3 +39,54 @@ func TestEncodeSearchMovieGRPCRequest(t *testing.T) {
 	})
 }
 
+func TestDecodeSearchMovieGRPCResponse(t *testing.T) {
+	t.Run("positive", func(t *testing.T) {
+		dummyRes := &movieProto.SearchResponse{
+			Response:     "True",
+			TotalResults: "375",
+			Search: []*movieProto.SearchResponse_MovieItem{
+				{
+					Title:  "Batman Beyond: Return of the Joker",
+					Year:   "2000",
+					ImdbID: "tt0233298",
+					Type:   "movie",
+					Poster: "https://m.media-amazon.com/images/M/MV5BNmRmODEwNzctYzU1MS00ZDQ1LWI2NWMtZWFkNTQwNDg1ZDFiXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_SX300.jpg",
+				},
+			},
+		}
+		expectedRes := SearchMovieResponse{
+			MovieListPerPage: MovieListPerPage{
+				Response:     dummyRes.Response,
+				TotalResults: dummyRes.TotalResults,
+				Search: []MovieItem{
+					{
+						Title:  "Batman Beyond: Return of the Joker",
+						Year:   "2000",
+						ImdbID: "tt0233298",
+						Type:   "movie",
+						Poster: "https://m.media-amazon.com/images/M/MV5BNmRmODEwNzctYzU1MS00ZDQ1LWI2NWMtZWFkNTQwNDg1ZDFiXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_SX300.jpg",
+					},
+				},
+			},
+		}
+
+		ctx := context.Background()
+		resAfterTranslation, err := decodeSearchMovieGRPCResponse(ctx, dummyRes)
+
+		if err != nil {
+			t.Fatal("an error has occurred")
+		} else if !reflect.DeepEqual(expectedRes, resAfterTranslation) {
+			t.Fatal("unexpected output")
+		}
+	})
+	t.Run("negative", func(t *testing.T) {
+		dummyRes := SearchMovieRequest{}
+
+		ctx := context.Background()
+		_, err := decodeSearchMovieGRPCResponse(ctx, dummyRes)
+
+		if err == nil {
+			t.Fatal("an error should occur")
+		}
+	})
+}
