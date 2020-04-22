c421e56058741c05286e28ad912813abef0d6723
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-23 03:54:17 +0700

    feat(transport): add endpoint maker, request decoder, response encoder

---
+++ transport/movie_endpoint.go
@@ -0,0 +1,42 @@
+package transport
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/tech-showcase/api-gateway/model"
+	"github.com/tech-showcase/api-gateway/presenter"
+	"github.com/tech-showcase/api-gateway/service"
+	"net/http"
+)
+
+type (
+	MovieEndpoints struct {
+		SearchMovieEndpoint endpoint.Endpoint
+	}
+)
+
+func MakeSearchMovieHTTPEndpoint(s service.MovieService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(presenter.SearchMovieRequest)
+		result, err := s.Search(ctx, req.Keyword, req.PageNumber)
+		res := presenter.SearchMovieResponse{
+			SearchMovieResponse: model.SearchMovieResponse{
+				MovieListPerPage: result,
+			},
+		}
+		return res, nil
+	}
+}
+
+func decodeSearchMovieHTTPRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var req presenter.SearchMovieRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+func encodeHTTPResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
