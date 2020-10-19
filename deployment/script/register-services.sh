feba14c637e3a32df035af491ffd47818d040e45
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-19 21:52:08 +0700

    chore(deployment): remove consul config

--- deployment/script/register-services.sh
+++
@@ -1,9 +0,0 @@
-docker exec consul-client \
-  /bin/sh -c \
-  "echo '$(cat deployment/consul/entertainment-service.json)' > /consul/config/entertainment-service.json"
-docker exec consul-client \
-  /bin/sh -c \
-  "echo '$(cat deployment/consul/entertainment-service-2.json)' > /consul/config/entertainment-service-2.json"
-
-docker exec consul-client \
-  consul reload
