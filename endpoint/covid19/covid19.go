e71abb48f9fa4224197b10e83eb8659483e5bd46
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-31 19:56:09 +0700

    feat(endpoint): implement performance monitoring in covid19

--- endpoint/covid19/covid19.go
+++ endpoint/covid19/covid19.go
@@ -1,14 +1,9 @@
 package covid19
 
 import (
-	"github.com/go-kit/kit/sd"
-	"github.com/go-kit/kit/sd/lb"
 	stdopentracing "github.com/opentracing/opentracing-go"
 	"github.com/tech-showcase/api-gateway/endpoint"
-	"github.com/tech-showcase/api-gateway/middleware"
 	"github.com/tech-showcase/api-gateway/service"
-
-	"time"
 )
 
 type (
@@ -18,19 +13,10 @@ type (
 )
 
 func NewCovid19Endpoint(covid19Services []service.Covid19Service, tracer stdopentracing.Tracer) (covid19Endpoint Endpoint) {
-	endpointer := sd.FixedEndpointer{}
-	for _, covid19Service := range covid19Services {
-		getCovid19Endpoint := makeGetCovid19Endpoint(covid19Service)
-		getCovid19Endpoint = middleware.ApplyTracerServer("getCovid19-endpoint", getCovid19Endpoint, tracer)
-
-		endpointer = append(endpointer, getCovid19Endpoint)
-	}
-
-	balancer := lb.NewRoundRobin(endpointer)
-	retry := lb.Retry(3, 500*time.Millisecond, balancer)
+	getCovid19Endpoint := newGetCovid19FixedEndpoint(covid19Services, tracer)
 
 	covid19Endpoint.Get = endpoint.HTTPEndpoint{
-		Endpoint: retry,
+		Endpoint: getCovid19Endpoint,
 		Decoder:  decodeGetCovid19Request,
 		Encoder:  encodeResponse,
 	}
--- endpoint/covid19/get.go
+++ endpoint/covid19/get.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/sd"
+	"github.com/go-kit/kit/sd/lb"
+	stdopentracing "github.com/opentracing/opentracing-go"
 	"github.com/tech-showcase/api-gateway/helper"
+	"github.com/tech-showcase/api-gateway/middleware"
 	"github.com/tech-showcase/api-gateway/model/covid19"
 	"github.com/tech-showcase/api-gateway/service"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type (
@@ -18,6 +24,22 @@ type (
 		covid19.GetCovid19Response
 	}
 )
+
+func newGetCovid19FixedEndpoint(covid19Services []service.Covid19Service, tracer stdopentracing.Tracer) endpoint.Endpoint {
+	getCovid19Endpointer := sd.FixedEndpointer{}
+	for index, covid19Service := range covid19Services {
+		getCovid19Endpoint := makeGetCovid19Endpoint(covid19Service)
+		getCovid19Endpoint = middleware.ApplyTracerServer("getCovid19-endpoint", getCovid19Endpoint, tracer)
+		getCovid19Endpoint = middleware.ApplyMetrics("covid19_"+strconv.Itoa(index), "get", getCovid19Endpoint)
+
+		getCovid19Endpointer = append(getCovid19Endpointer, getCovid19Endpoint)
+	}
+
+	balancer := lb.NewRoundRobin(getCovid19Endpointer)
+	retry := lb.Retry(3, 500*time.Millisecond, balancer)
+
+	return retry
+}
 
 func makeGetCovid19Endpoint(covid19Service service.Covid19Service) endpoint.Endpoint {
 	getCovid19Endpoint := func(ctx context.Context, request interface{}) (response interface{}, err error) {
