12ca9d39423904a0556594bc347487a150455b09
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-31 19:43:34 +0700

    feat(middleware): add performance monitoring middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,9 +5,13 @@ import (
 	"github.com/go-kit/kit/circuitbreaker"
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	"github.com/go-kit/kit/tracing/opentracing"
 	stdopentracing "github.com/opentracing/opentracing-go"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 	"github.com/sony/gobreaker"
+	"github.com/tech-showcase/api-gateway/config"
+	"strings"
 	"time"
 )
 
@@ -37,6 +41,34 @@ func ApplyLogger(operationName string, nextEndpoint endpoint.Endpoint, logger lo
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
 		defer func(begin time.Time) {
 			logger.Log("operationName", operationName, "elapsedTime", time.Since(begin))
+		}(time.Now())
+
+		return nextEndpoint(ctx, request)
+	}
+}
+
+func ApplyMetrics(subsystem, operation string, nextEndpoint endpoint.Endpoint) endpoint.Endpoint {
+	var labelNames []string
+	namespace := strings.Replace(config.Instance.ServiceName, "-", "_", -1)
+
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: namespace,
+		Subsystem: subsystem,
+		Name:      operation + "_request_count",
+		Help:      "Number of requests received.",
+	}, labelNames)
+
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: namespace,
+		Subsystem: subsystem,
+		Name:      operation + "_request_latency_seconds",
+		Help:      "Total duration of requests in seconds.",
+	}, labelNames)
+
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		requestCount.Add(1)
+		defer func(begin time.Time) {
+			requestLatency.Observe(time.Since(begin).Seconds())
 		}(time.Now())
 
 		return nextEndpoint(ctx, request)
